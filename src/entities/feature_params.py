3a24b5863abfcd0949e0731fc6d4dfe5b8089fd3
Artem Suchkov <artem.suchkov@technologiivokrug.ru>
2023-10-18 13:29:10 +0300

    update files

--- src/entities/feature_params.py
+++
@@ -1,9 +0,0 @@
-from dataclasses import dataclass, field
-from typing import List
-
-
-@dataclass
-class FeatureParams:
-    count_features: List[str]
-    ctr_features: List[str]
-    target_col: str = field(default="target")
