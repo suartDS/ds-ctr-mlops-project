220d38784c53442992d94af5364ff96abaa6fe9f
Artem Suchkov <artem.suchkov@technologiivokrug.ru>
2023-10-24 02:32:35 +0300

    add dvc config

---
+++ data/raw/download_data.py
@@ -0,0 +1,34 @@
+import os
+import boto3
+from dotenv import load_dotenv
+
+if __name__ == "__main__":
+    load_dotenv()
+    
+    aws_access_key_id = os.getenv("aws_access_key_id")
+    aws_secret_access_key = os.getenv("aws_secret_access_id")
+    
+    print("aws_access_key_id", aws_access_key_id)
+    print("aws_secret_access_key", aws_secret_access_key)
+    
+    session = boto3.session.Session()
+    
+    s3_client = session.client(
+        service_name = "s3",
+        region_name = "ru-msk",
+        aws_access_key_id = aws_access_key_id,
+        aws_secret_access_key = aws_secret_access_key,
+        endpoint_url = "http://hb.ru-msk.vkcs.cloud"
+    )
+
+    #response = s3_client.list_buckets()
+
+    #for key in response['Buckets']:
+    #    print(key['Name'])
+    #"../data/raw/sampled_train_50k.csv"
+    s3_client.download_file(
+                    "ctr_cnt",
+                    "tests/sample_train_50k.csv",
+                    "ctr_cnt",
+                    "data/sample_train_50k_v_2.csv"
+                )
